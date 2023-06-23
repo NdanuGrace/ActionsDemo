01b42b158843cc8a8b508226fe7cbdf82bf698ac
NdanuGrace <gracendanu27@gmail.com>
2023-06-23 08:55:38 +0300

    install node fetch

--- updateNotion.js
+++ updateNotion.js
@@ -1,8 +1,7 @@
 const fetch = require('node-fetch');
 
 const NOTION_API_TOKEN = process.env.NOTION_API_TOKEN;
-const NOTION_PAGE_ID = '9c4e25b41bf049da9206a06e8c8626c2';
-
+const NOTION_PAGE_ID = '27ecc59850a749f9846cd1466d701d66';
 async function updateNotionPage(content) {
   const headers = {
     'Authorization': `Bearer ${NOTION_API_TOKEN}`,
